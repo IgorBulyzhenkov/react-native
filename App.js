6952ac36c793815b3ff50b3f04b194bf660ad90d
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-10-06 20:40:06 +0300

    update

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>My project!</Text>
+      <View style={styles.border}></View>
+      <Button title={"Login"} style={styles.input} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,11 +16,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000",
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
   text: {
     color: "green",
+    fontSize: 50,
+  },
+  border: {
+    borderRadius: 50,
+    backgroundColor: "#000",
   },
 });
